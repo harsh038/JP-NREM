110f2a21be016dc4b1f4dde00be71f5d03a60937
Harsh Thummar <harshthummar2004@gmail.com>
2025-02-11 23:22:53 +0530

    Context Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import ApplyJob from "./pages/ApplyJob";
+import Applications from "./pages/Applications";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/apply-job/:id" element={<ApplyJob />} />
+        <Route path="/applications" element={<Applications />} />
       </Routes>
     </div>
   );
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import { createContext } from "react"
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props)=>{
+    const value={}
+    return(
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+}
