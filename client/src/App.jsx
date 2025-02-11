2b1d3d635d2b4b4e3ee0e6e23bbb80a94c309da5
Harsh Thummar <harshthummar2004@gmail.com>
2025-02-11 22:49:40 +0530

    pages initialize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 const App = () => {
-  return <div className="bg-amber-400">APP</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
