cb0bda4ccfb638e9fc72f6160dde5cc86a6e4b4c
Harsh Thummar <harshthummar2004@gmail.com>
2025-02-12 20:26:22 +0530

    hero section complete

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,18 @@
+import { useContext, useRef } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 const Hero = () => {
+  const { setSearchFilter, setIsSearched } = useContext(AppContext);
+  const titleRef = useRef(null);
+  const locationRef = useRef(null);
+  const onSearch = () => {
+    setSearchFilter({
+      title: titleRef.current.value,
+      location: locationRef.current.value,
+    });
+    setIsSearched(true);
+  };
+
   return (
     <div className="container 2xl:px-20 mx-auto  my-10">
       <div className="bg-gradient-to-r from-purple-800 to-purple-950 text-white py-16 text-center rounded-2xl">
@@ -14,6 +27,7 @@ const Hero = () => {
           <div className="flex items-center">
             <img className="h-4 sm:h-5" src={assets.search_icon} alt="" />
             <input
+              ref={titleRef}
               type="text"
               placeholder="Search for a jobs"
               className="max-sm:text-xs p-2 rounded outline-none w-full"
@@ -22,28 +36,31 @@ const Hero = () => {
           <div className="flex items-center">
             <img className="h-4 sm:h-5" src={assets.location_icon} alt="" />
             <input
+              ref={locationRef}
               type="text"
               placeholder="location"
               className="max-sm:text-xs p-2 rounded outline-none w-full"
             />
           </div>
-          <button className="bg-blue-600 text-white rounded px-6 py-2 m-1">
+          <button
+            className="bg-blue-600 text-white rounded px-6 py-2 m-1"
+            onClick={onSearch}
+          >
             Search
           </button>
         </div>
-        
       </div>
-      <div className="border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex  " > 
-          <div className="flex justify-center gap-10 lg:gap-16 flex-wrap">
-            <p className="font-medium">Trusted by</p>
-            <img className="h-6" src={assets.walmart_logo} alt="" />
-            <img className="h-6" src={assets.microsoft_logo} alt="" />
-            <img className="h-6" src={assets.accenture_logo} alt="" />
-            <img className="h-6" src={assets.samsung_logo} alt="" />
-            <img className="h-6" src={assets.amazon_logo} alt="" />
-            <img className="h-6" src={assets.adobe_logo} alt="" />
-          </div>
+      <div className="border border-gray-300 shadow-md mx-2 mt-5 p-6 rounded-md flex  ">
+        <div className="flex justify-center gap-10 lg:gap-16 flex-wrap">
+          <p className="font-medium">Trusted by</p>
+          <img className="h-6" src={assets.walmart_logo} alt="" />
+          <img className="h-6" src={assets.microsoft_logo} alt="" />
+          <img className="h-6" src={assets.accenture_logo} alt="" />
+          <img className="h-6" src={assets.samsung_logo} alt="" />
+          <img className="h-6" src={assets.amazon_logo} alt="" />
+          <img className="h-6" src={assets.adobe_logo} alt="" />
         </div>
+      </div>
     </div>
   );
 };
