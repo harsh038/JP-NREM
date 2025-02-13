6ab57b692d9af7dcf0aeaeeb41f7923f4dcd9f6e
Harsh Thummar <harshthummar2004@gmail.com>
2025-02-13 20:04:01 +0530

    footer done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import AppDownload from "../components/AppDownload";
+import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import JobListing from "../components/JobListing";
 import Navbar from "../components/Navbar";
@@ -9,6 +10,7 @@ const Home = () => {
     <Hero/>
     <JobListing/>
     <AppDownload/>
+    <Footer/>
   </div>;
 };
 
