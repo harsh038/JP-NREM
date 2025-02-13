49445b3d2de6ef18da831da91b17622b3e2a5914
Harsh Thummar <harshthummar2004@gmail.com>
2025-02-13 19:52:55 +0530

    app banner added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import AppDownload from "../components/AppDownload";
 import Hero from "../components/Hero";
 import JobListing from "../components/JobListing";
 import Navbar from "../components/Navbar";
@@ -7,6 +8,7 @@ const Home = () => {
     <Navbar/>
     <Hero/>
     <JobListing/>
+    <AppDownload/>
   </div>;
 };
 
