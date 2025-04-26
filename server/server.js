32174893335cdbc39b22035309a64c6544f79dee
Harsh Thummar <harshthummar2004@gmail.com>
2025-04-26 20:16:46 +0530

    routes added

--- server/server.js
+++ server/server.js
@@ -5,17 +5,30 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import Users from "./models/User.js";
+import bodyParser from "body-parser";
+import AuthRouter from "./routes/AuthRouter.js";
+import UserRouter from "./routes/UserRoutes.js";
 
 const app = express();
 
 await connectDB();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(bodyParser.json());
+app.use("/auth", AuthRouter);
+app.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
 });
+
 // app.get("/debug-sentry", function mainHandler(req, res) {
 //   throw new Error("My first Sentry error!");
 // });
