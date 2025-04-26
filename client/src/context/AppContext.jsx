3a45325695f703b662c52d2c4cabbe3ce5057bab
Harsh Thummar <harshthummar2004@gmail.com>
2025-04-26 20:06:10 +0530

    remove clerk

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,33 +5,31 @@ import { jobsData } from "../assets/assets";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  const [searchFilter, setSearchFilter] = useState(
-    {
-      title:"",
-      location:""
-    }
-  );
+  const [searchFilter, setSearchFilter] = useState({
+    title: "",
+    location: "",
+  });
 
-  const [isSearched,setIsSearched]= useState(false)
+  const [isSearched, setIsSearched] = useState(false);
+  const [jobs, setJobs] = useState([]);
 
-  const [jobs,setJobs]=useState([])
+  const fetchJobs = async () => {
+    await setJobs(jobsData);
+  };
 
-  const [showRecruiterLogin,setShowRecruiterLogin]=useState(false)
-
-  // func to fetch job data
-  const fetchJobs = async ()=>{
-    await setJobs(jobsData)
-  }
-  useEffect(()=>{
-        fetchJobs()
-  },[])
+  useEffect(() => {
+    fetchJobs();
+  }, []);
 
   const value = {
-    searchFilter,setSearchFilter,
-    isSearched,setIsSearched,
-    jobs,setJobs,
-    showRecruiterLogin,setShowRecruiterLogin,
+    searchFilter,
+    setSearchFilter,
+    isSearched,
+    setIsSearched,
+    jobs,
+    setJobs,
   };
+
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
